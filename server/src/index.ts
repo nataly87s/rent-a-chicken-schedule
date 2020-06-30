95dbc1e67752fd9171c1cccb09e334630e7fe7eb
Natalya Shrits <nataly@soluto.com>
2020-06-30 13:35:21 -0500

    fix client location

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,11 +8,11 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../../client/build')));
 
     // Handle React routing, return all requests to React app
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
     });
 }
 

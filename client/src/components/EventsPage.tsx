5a95e99131b455f8ebdc09969a16e78d7e2abac9
Natalya Shrits <nataly87s@gmail.com>
2020-07-01 23:46:13 -0500

    filter events

--- client/src/components/EventsPage.tsx
+++ client/src/components/EventsPage.tsx
@@ -1,40 +1,95 @@
-import React from 'react';
+import React, {useState} from 'react';
+import add from 'date-fns/add';
+import isBefore from 'date-fns/isBefore';
 import Box from '@material-ui/core/Box';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
 import {makeStyles} from '@material-ui/core/styles';
 import {useStore} from '../context/StoreContext';
 import EventCard from './EventCard';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
     box: {
-        padding: '3em',
+        padding: '2em 3em',
+        flexDirection: 'column',
+    },
+    cards: {
+        marginTop: '2em',
         display: 'grid',
         gridTemplateColumns: 'repeat(2, 1fr)',
         gridAutoColumns: 'auto',
         gridGap: '8px',
     },
+    radioGroup: {
+        flexDirection: 'row',
+    },
 });
+
+enum ViewType {
+    all,
+    week,
+    month,
+}
 
 export type EventsPageProps = {
     customerId?: number;
 };
 
+const nextMonth = add(Date.now(), {months: 1});
+const nextWeek = add(Date.now(), {weeks: 1});
+
 const EventsPage = ({customerId}: EventsPageProps) => {
+    const [viewType, setViewType] = useState(ViewType.all);
     const {events, customers} = useStore();
     const classes = useStyles();
-    const filterByCustomer = typeof customerId === 'number';
+    const filterByCustomer = customers.find((c) => c.id === customerId);
+
+    let filteredEvents = events;
+    if (filterByCustomer) {
+        filteredEvents = filteredEvents.filter((e) => e.customerId === customerId);
+    }
+
+    switch (viewType) {
+        case ViewType.month:
+            filteredEvents = filteredEvents.filter((e) => isBefore(e.start, nextMonth));
+            break;
+        case ViewType.week:
+            filteredEvents = filteredEvents.filter((e) => isBefore(e.start, nextWeek));
+            break;
+    }
+
     return (
         <Box className={classes.box}>
-            {filterByCustomer && <EventCard customer={customers.find((c) => c.id === customerId)!} />}
+            {filterByCustomer && (
+                <Typography variant={'h4'} component={'h1'}>
+                    {filterByCustomer.firstName} {filterByCustomer.lastName}
+                </Typography>
+            )}
+            <FormControl component="fieldset">
+                <RadioGroup
+                    className={classes.radioGroup}
+                    value={viewType}
+                    onChange={(e) => setViewType(Number(e.target.value))}
+                >
+                    <FormControlLabel value={ViewType.all} control={<Radio />} label="View All" />
+                    <FormControlLabel value={ViewType.week} control={<Radio />} label="View Week" />
+                    <FormControlLabel value={ViewType.month} control={<Radio />} label="View Month" />
+                </RadioGroup>
+            </FormControl>
+            <Box className={classes.cards}>
+                {filterByCustomer && <EventCard customer={filterByCustomer} />}
 
-            {events
-                .filter((e) => !filterByCustomer || customerId == e.customerId)
-                .map((event) => (
+                {filteredEvents.map((event) => (
                     <EventCard
                         key={event.id}
                         event={event}
                         customer={customers.find((c) => c.id === event.customerId)!}
                     />
                 ))}
+            </Box>
         </Box>
     );
 };

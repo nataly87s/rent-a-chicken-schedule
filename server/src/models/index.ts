5bbef8ae7e3f72cf01c0572942941d2281d657ba
Natalya Shrits <nataly87s@gmail.com>
2020-07-03 15:08:21 -0500

    add logs

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -6,6 +6,9 @@ export * from './types';
 
 const sequelizeConfig = process.env.NODE_ENV === 'production' ? config.production : config.development;
 
+console.log('startup', process.env.NODE_ENV, sequelizeConfig);
+console.log('DATABASE_URL', process.env.DATABASE_URL);
+
 export const sequelize = new Sequelize(sequelizeConfig as Options);
 
 initialize(sequelize);

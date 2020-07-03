94311453b0eeefca61deb44a6ca4b22d63d2835b
Natalya Shrits <nataly87s@gmail.com>
2020-07-03 17:57:55 -0500

    seed data

--- migrations/202006302200-create-customers-and-events.js
+++ migrations/202006302200-create-customers-and-events.js
@@ -41,6 +41,35 @@ module.exports = {
             },
         });
 
+        await queryInterface.bulkInsert('Customers', [
+            {
+                firstName: 'Natalya',
+                lastName: 'Shrits',
+                phoneNumber: '615-278-0965',
+                email: 'nshrits@wgu.edu',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
+                firstName: 'Billy',
+                lastName: 'Rabbit',
+                phoneNumber: '564-987-9909',
+                email: 'thebestrabbitinthewest@gmail.com',
+                notes: 'Old Billy is a pretty cool guy but he eats way too many carrots.',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
+                firstName: 'Winnie',
+                lastName: 'Pooh',
+                phoneNumber: '337-437-9776',
+                email: 'winnie.pooh@disney.com',
+                notes: 'Loves honey',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+        ]);
+
         await queryInterface.createTable('Events', {
             id: {
                 allowNull: false,
@@ -78,6 +107,61 @@ module.exports = {
                 type: Sequelize.DATE,
             },
         });
+
+        await queryInterface.bulkInsert('Events', [
+            {
+                customerId: 1,
+                address: 'Lorem ipsum dolor sit amet',
+                start: new Date(2020, 5, 1, 9),
+                end: new Date(2020, 5, 1, 11),
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
+                customerId: 1,
+                address: 'consectetur adipiscing elit',
+                start: new Date(2020, 5, 14, 14),
+                end: new Date(2020, 5, 14, 18),
+                notes: 'Excepteur sint occaecat cupidatat',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
+                customerId: 1,
+                address: 'The beach',
+                start: new Date(2020, 11, 8, 9),
+                end: new Date(2020, 11, 8, 13),
+                notes: 'Birthday party',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
+                customerId: 2,
+                address: 'Nissan Stadium',
+                start: new Date(2020, 6, 4, 19),
+                end: new Date(2020, 6, 4, 20),
+                notes: '4th of July fireworks',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
+                customerId: 2,
+                address: 'sed do eiusmod tempor incididunt',
+                start: new Date(2020, 5, 7, 10),
+                end: new Date(2020, 5, 7, 13),
+                notes: 'ad minim veniam, quis nostrud exercitation',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
+                customerId: 3,
+                address: 'ut labore et dolore magna aliqua',
+                start: new Date(2020, 5, 20, 16),
+                end: new Date(2020, 5, 20, 18),
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+        ]);
     },
     down: async (queryInterface, Sequelize) => {
         await queryInterface.dropTable('Events');

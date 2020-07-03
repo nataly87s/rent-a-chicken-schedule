ad3b617d9d681587ad930a7a3907d3c04b50112b
Natalya Shrits <nataly87s@gmail.com>
2020-07-03 01:22:41 -0500

    user is a model

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -2,11 +2,8 @@ import {Model} from 'sequelize';
 import {ModelAttributes, ModelCreationAttributes} from './types';
 
 export interface UserAttributes extends ModelAttributes {
-    id: number;
     userName: string;
     password: string;
-    createdAt: Date;
-    updatedAt: Date;
 }
 
 export default class User extends Model<UserAttributes, ModelCreationAttributes<UserAttributes>>
